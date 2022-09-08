f4d7a5ee5281a34e269afc6aa9884599b3ee5f6e
trbl <domi@outlook.at>
2022-09-08 16:45:20 +0200

    delete comment support

--- Program.cs
+++ Program.cs
@@ -32,14 +32,20 @@ internal class Program
     public static string CommentStoragePath = "/srv/gemini/her.st/blog/comments.json";
     public static Dictionary<string, List<Comment>> Posts = new();
 
+    public static string Target = "";
+
     private static void Main()
     {
         var kvp = pathInfo.Split('/', StringSplitOptions.RemoveEmptyEntries);
         foreach (var str in kvp)
             Console.Error.WriteLine(str);
 
+        Console.Error.WriteLine("Cert from: " + certFrom);
+
         var type = kvp[1];
-        var target = kvp[0];
+        Target = kvp[0];
+
+        Console.Error.WriteLine($"Type {type} Target {Target}");
 
         if (File.Exists(CommentStoragePath))
         {
@@ -47,37 +53,53 @@ internal class Program
             Posts = JsonSerializer.Deserialize<Dictionary<string, List<Comment>>>(json);
         }
 
-        foreach (var file in Directory.GetFiles(SourcePath))
+        switch (type)
         {
-            var post = Path.GetFileName(file);
-
-            if (post != target)
-                continue;
-
-            switch (type)
-            {
-                case "view":
+            case "view":
+                {
+                    foreach (var file in Directory.GetFiles(SourcePath))
                     {
+                        var post = Path.GetFileName(file);
+
+                        if (post != Target)
+                            continue;
+
                         Console.Write("20 text/gemini\r\n");
-                        Console.Write("### Comments\n");
-                        Console.Write("> 🪪 Your Certificate Subject (CN=) will be use for your name and the Certificate Thumbprint will be saved & publicly accessible.\n");
-                        Console.Write($"=> {geminiUrl.Replace("view", "add")} add one!\n");
-                        foreach (var comments in Posts.Where(x => x.Key == target).Select(x => x.Value))
+                        if (authType.ToUpperInvariant() == "CERTIFICATE")
+                        {
+                            Console.Write($"> 🪪 Subject: {remoteUser}\n");
+                            Console.Write($"> 🪪 Thumbprint: {certHash}\n");
+                            Console.Write($"Your comments will be attributed to the Subject of your certificate as shown above. The Thumbprint will be stored along with it so you can edit/delete your comment later.\n");
+                            Console.Write($"=> {geminiUrl.Replace("view", "add")} add one!\n");
+                        }
+                        else
+                            Console.Write($"> 🪪 No Certificate! You can only read comments but not write any.\n");
+
+                        Console.Write("## Comments\n");
+                        foreach (var comments in Posts.Where(x => x.Key == Target).Select(x => x.Value))
                         {
                             foreach (var comment in comments)
                             {
                                 foreach (var line in comment.Text.Split('\n'))
                                     Console.Write($"> {line}\n");
                                 Console.Write($"> -- {comment.Username} at {comment.TimeStamp}\n");
-                                Console.WriteLine();
+                                if (comment.Thumbprint == certHash)
+                                    Console.WriteLine($"=> {geminiUrl.Replace("view", "delete").Replace(Target, comment.Id)} delete\n");
                                 Console.WriteLine();
                             }
                         }
-                        Console.Write($"### End\n");
                         break;
                     }
-                case "add":
+                    break;
+                }
+            case "add":
+                {
+                    foreach (var file in Directory.GetFiles(SourcePath))
                     {
+                        var post = Path.GetFileName(file);
+
+                        if (post != Target)
+                            continue;
                         if (authType.ToLowerInvariant() != "certificate")
                         {
                             Console.Write($"60 Commenting requires a client certificate\r\n");
@@ -95,15 +117,14 @@ internal class Program
                         }
                         else
                         {
-                            if (!Posts.TryGetValue(target, out var commentList))
+                            if (!Posts.TryGetValue(Target, out var commentList))
                             {
                                 commentList = new();
-                                Posts.Add(target, commentList);
+                                Posts.Add(Target, commentList);
                             }
 
                             var text = HttpUtility.UrlDecode(query[1..]);
-
-                            commentList.Add(new Comment(certSubject, certHash, text));
+                            commentList.Add(new Comment(certSubject, certHash, text, Target));
 
                             var json = JsonSerializer.Serialize(Posts, new JsonSerializerOptions { WriteIndented = true });
                             File.WriteAllText(CommentStoragePath, json);
@@ -111,16 +132,42 @@ internal class Program
                         }
                         break;
                     }
-                case "delete":
+                    break;
+                }
+            case "delete":
+                {
+                    var postList = default(List<Comment>);
+                    var oldComment = default(Comment);
+
+                    Console.Error.WriteLine("Searching " + Target);
+                    foreach (var list in Posts.Values)
+                        foreach (var comment in list)
+                            if (comment.Id == Target)
+                            {
+                                oldComment = comment;
+                                postList = list;
+                                Console.Error.WriteLine("Found " + Target);
+                                break;
+                            }
+
+                    if (oldComment != null)
                     {
-                        break;
+                        Console.Error.WriteLine("Deleting " + Target);
+                        if (oldComment.Thumbprint == certHash)
+                            postList.Remove(oldComment);
+                        var newUrl = geminiUrl.Replace("delete", "view").Replace(oldComment.Id, oldComment.File);
+                        var json = JsonSerializer.Serialize(Posts, new JsonSerializerOptions { WriteIndented = true });
+                        File.WriteAllText(CommentStoragePath, json);
+                        Console.Error.WriteLine("Redirect to " + newUrl);
+                        Console.Write($"30 {newUrl}\r\n");
                     }
-                case "update":
+                    else
                     {
-                        break;
+                        Console.Error.WriteLine("Redirect to  " + serverName);
+                        Console.Write($"30 {serverName}\r\n");
                     }
-            }
-            break;
+                    break;
+                }
         }
     }
 }
